f262222aafa97a65d49dfb9d7e5f8c6a0c35c07e
const-nishant <nishantspatil04@gmail.com>
2025-03-07 14:50:42 +0530

    user models and text field

---
+++ lib/common/commontextfield.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Commontextfield extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final String hintText;
+  final bool obscureText;
+  final bool readOnly;
+  final FocusNode focusNode;
+  final int maxLength;
+  final String? Function(String?)? validator;
+  final Function()? onTap;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+
+  const Commontextfield(
+      {super.key,
+      required this.controller,
+      required this.keyboardType,
+      required this.hintText,
+      required this.obscureText,
+      required this.readOnly,
+      required this.focusNode,
+      required this.maxLength,
+      this.validator,
+      this.onTap,
+      this.prefixIcon,
+      this.suffixIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      focusNode: focusNode,
+      maxLength: maxLength,
+      obscureText: obscureText,
+      onTap: onTap,
+      controller: controller,
+      keyboardType: keyboardType,
+      validator: validator,
+      style: TextStyle(
+        color: Theme.of(context).colorScheme.primary,
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+      ),
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        prefixIcon: prefixIcon,
+        prefixIconColor: Theme.of(context).colorScheme.primary,
+        suffixIcon: suffixIcon,
+        suffixIconColor: Colors.black,
+        counterText: "",
+        enabledBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary, width: 1.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Theme.of(context).colorScheme.tertiary,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        fillColor: Colors.transparent,
+        filled: true,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 10.0,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/auth/data/models/usermodel.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String? username;
+  final String? email;
+  final String uid;
+  final String? walletAddress;
+  final String? menoics;
+  final String? notificationId;
+  final bool isSignup;
+  final String authProvider;
+
+  UserModel({
+    required this.username,
+    required this.email,
+    required this.uid,
+    required this.walletAddress,
+    required this.menoics,
+    required this.notificationId,
+    required this.isSignup,
+    required this.authProvider,
+  });
+
+  // Convert object to Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'username': username,
+      'email': email,
+      'uid': uid,
+      'walletAddress': walletAddress,
+      'menoics': menoics,
+      'notificationId': notificationId,
+      'isSignup': isSignup,
+      'authProvider': authProvider,
+    };
+  }
+
+  // Create an object from Firestore document
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      username: map['username'] ?? '',
+      email: map['email'] ?? '',
+      uid: map['uid'] ?? '',
+      walletAddress: map['walletAddress'] ?? '',
+      menoics: map['menoics'] ?? '',
+      notificationId: map['notificationId'] ?? '',
+      isSignup: map['isSignup'] ?? false,
+      authProvider: map['authProvider'] ?? '',
+    );
+  }
+
+  // Create an object from Firestore document snapshot
+  factory UserModel.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return UserModel.fromMap(data);
+  }
+}
