3f1a734434b7acba7396ab762c26356c26e275e2
const-nishant <nishantspatil04@gmail.com>
2025-03-07 15:17:06 +0530

    made changes in the main file so the app can run

---
+++ lib/config/configs.dart
@@ -0,0 +1,5 @@
+class Configs {
+  static const String appWriteEndpoint = 'https://api.syncora.io/v1';
+  static const String appWriteProjectId = '67caa95d0032583bcf78';
+  static const String appWriteUserImages = '67caa9b3001d50649df9';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:provider/provider.dart';
+import 'package:syncora_application/config/configs.dart';
+import 'package:syncora_application/firebase_options.dart';
 import 'package:syncora_application/modules/auth/data/services/authwrapper.dart';
+import 'modules/auth/data/services/authservices.dart';
+import 'modules/home/data/services/walletaddressservices.dart';
 
-void main() {
-  runApp(const MyApp());
+Client client = Client();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  client
+      .setEndpoint(Configs.appWriteEndpoint)
+      .setProject(Configs.appWriteProjectId);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthServices(),
+        ),
+        ChangeNotifierProvider(create: (_) => WalletProvider()),
+      ],
+      child: Phoenix(
+        child: MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +39,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Syncora',
+      debugShowCheckedModeBanner: false,
       // theme: ,
       home: Authwrapper(),
     );
